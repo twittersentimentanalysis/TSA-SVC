f8f61ee2b1387567ee9f145305d801c297eb7406
Ariadna de Arriba <aridease@gmail.com>
2021-06-10 22:08:12 +0200

    some improvements and bugs fixed

--- Classifier.py
+++ Classifier.py
@@ -1,23 +1,21 @@
 import json
-import pandas as pd
 
+# Get emotions probabilities for text recieved
 def get_emotion(text, count_vect, tf_transformer, calibrated_svc, label_dict):
     # read configuration file
-    js = open('config.json').read()
+    js = open('config-api.json').read()
     config = json.loads(js)
 
     # predict emotions
     to_predict = [text]
     p_count = count_vect.transform(to_predict)
     p_tfidf = tf_transformer.transform(p_count)
-    # print('Average accuracy on test set={}'.format(np.mean(predicted == labels.transform(y_test))))
-    # print('Predicted probabilities of demo input string are')
     result = calibrated_svc.predict_proba(p_tfidf)
     print(result)
 
-    # Printing predicted probability in json
+    # printing predicted probability in json
     emotions_prob = {}
-    possible_labels = config['possible-labels']
+    possible_labels = config['label-dict']
 
     for index, possible_label in enumerate(possible_labels):
         index = label_dict[possible_label]
